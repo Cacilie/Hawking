07da484e59aa5bc92bcb0cc138fe90b2698c6a2b
Edgar <hi.ed@hotmail.com>
2018-04-29 17:54:54 -0500

    eye_aspect_ratio function

--- detect_blinks.py
+++ detect_blinks.py
@@ -11,3 +11,22 @@ import cv2
 
 
 print "ready"
+
+
+def eye_aspect_ratio(ete):
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+    C = dist.euclidean(eye[0], eye[3])
+
+
+    ear = (A + B) / (2.0 * C)
+
+    return ear
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True,
+help="path to facial ladmark predictor")
+ap.add_argument("-v", "--video", type=str, defaul="",
+help="path to input video file")
+
+arg = vars(ap.parse_args())
